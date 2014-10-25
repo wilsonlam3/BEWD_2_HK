df9d0ffed8ad2b1d2e07eb8cd6b37cef1bf8e67b
yeechan288 <yeechan288@gmail.com>
2014-10-25 16:31:51 +0800

    Update ex_teddit_conditional.rb

--- lessons/02_Variables_Conditionals/exercises/ex_teddit_conditional.rb
+++ lessons/02_Variables_Conditionals/exercises/ex_teddit_conditional.rb
@@ -5,17 +5,18 @@
 # Where you see comments (lines that begin with #) replace it with code so that the program works.
 
 def get_input
-  #Get input from the user.
+  gets
 end
 
-def calculate_upvotes(story, category)
-	# Write code so that:
-		# If the Story is about cats multiply the upvotes by 5
-		# If the Story is about bacon multiply the upvotes by 8
-		# If the Story is about Food it gets 3 times the upvotes.
-
-	#For example:
-	# "Cats frolic despite tuna shortage" should give you 5 times the upvotes!
+def calculate_upvotes(story, category, upvotes)
+	if category.include? "cats"
+		upvotes * 5
+	elseif category.include? "bacon"
+		upvotes * 8
+	elseif category.include? "food"
+		upvotes *3
+	else 
+		upvotes
 end
 
 puts "Welcome to Teddit! a text based news aggregator. Get today's news tomorrow!"

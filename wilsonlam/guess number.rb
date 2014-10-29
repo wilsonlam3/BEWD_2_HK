25ba85c90d11efddc211e1495c971c3cdf1686f5
wilsonlam3 <wclam3@gmail.com>
2014-10-29 15:51:33 +0800

    update guess number.rb file

--- wilsonlam/guess number.rb
+++ wilsonlam/guess number.rb
@@ -1,6 +1,6 @@
 
 
-attempts = 1
+attempts = 0
 secret_number = 8
 
 def get_input
@@ -8,29 +8,23 @@ def get_input
 end
 
 def check_guess(n, secret_number, attempts)
-	
 	if n < secret_number
 		puts "Higher...Please try again"
 	elsif n > secret_number
 		puts "Lower...Please try again"
 	elsif n = secret_number
 		puts "You guessed it right! The secret number was #{secret_number} \nYou guessed it in #{attempts} attempts"
+		exit
 	end
-	
-	attempts ++ 1
-
-	if attempts == 6
-		puts "Sorry you had reached the maximum number of tries"
-		puts "The secret number was #{secret_number}"
-	end
-
 end
-
 
 puts "Welcome, guess my secret number"
 puts "You have only 5 attempts to guess it right!"
-n = get_input.to_i
-
-number = check_guess(n, secret_number, attempts)
-
-
+while attempts < 5
+	n = get_input.to_i
+	attempts +=1
+	number = check_guess(n, secret_number, attempts)
+end
+puts "Sorry you had reached the maximum number of tries"
+puts "The secret number was #{secret_number}"		
+exit

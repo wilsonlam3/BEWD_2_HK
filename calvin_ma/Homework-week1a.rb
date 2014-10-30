345169287ad3239ced6d3c6d13636de003f870d3
calvin.ma <calvinhcma@gmail.com>
2014-10-30 22:54:26 +0800

    updated Homework-week1a.rb

--- calvin_ma/Homework-week1a.rb
+++ calvin_ma/Homework-week1a.rb
@@ -1,17 +1,22 @@
 puts "Please guess a number from 1 to 5."
 guess = rand(5)+1
 
-def get_input
-gets.chomp.to_i
-end
-
 loop do
-	if get_input == guess
+
+	def get_input
+	gets.chomp.to_i
+	end
+
+	user_guess = get_input
+
+	if guess == user_guess
 	puts "Yes! Correct answer!"
 break
-	elsif get_input > guess 
+	elsif user_guess > guess && user_guess <= 5
 	puts "Too high. Try again."
-	elsif get_input < guess
+	elsif user_guess < guess && user_guess >= 1
 	puts "Too low. Try again."
+	else
+	puts "Only input a number from 1 to 5!!"
 	end
 end

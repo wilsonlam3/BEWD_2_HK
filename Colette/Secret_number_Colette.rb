f9b42b54149d6700bcd46d9771fe55cc89bd0e92
Hubert Lee <hubert.k.lee@gmail.com>
2014-10-30 17:32:44 +0800

    update Colette's hw

--- Colette/Secret_number_Colette.rb
+++ Colette/Secret_number_Colette.rb
@@ -4,24 +4,24 @@ def get_input
 	gets
 end
 
-def process_guess(guess,result)
-	if guess > 10 && guess < 1
-		result = "1 to 10, ok?"
+def process_guess(guess)
+	if guess > 10 || guess < 1
+		"1 to 10, ok?"
 	elsif guess == Secret_number
-		result = "Correct! Are you a mind reader?"
+    "Correct! Are you a mind reader?"
 	elsif guess > Secret_number && guess <= 10
-		result = "High as a kite."
+		"High as a kite."
 	elsif guess < Secret_number && guess >= 1
-		result = "Why so low?"
+		"Why so low?"
 	else
-		result = "You should google what is an integer."
+		"You should google what is an integer."
 	end
 end
 
 Secret_number = 6
 puts "Try and guess my secret number?"
 puts "Input an integer from 1 to 10. Decimal places are rounded down."
-guess = get_input.to_i
-result = process_guess(guess,Secret_number)
+user_guess = get_input.to_i
+result = process_guess(user_guess)
 puts "I have news for you... #{result}"
 

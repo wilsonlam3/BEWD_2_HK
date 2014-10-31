b7d3725b17b572366f199c96834fb26a423a5169
Hubert Lee <hubert.k.lee@gmail.com>
2014-11-01 02:00:44 +0800

    add homework for lessons 3 and 4

---
+++ lessons/04_Collections_APIs/homework/apis.rb
@@ -0,0 +1,16 @@
+###############################################################################
+#
+# Back-End Web Development - Homework #3
+#
+# The purpose of this homework is to practice manipulating data retrieved from
+# third-party APIs.
+#
+# 1. Retrieve the list of HK government notifications from the API described in:
+#      http://www.gov.hk/en/theme/psi/datasets/govhknotilist.htm
+# 2. Use the data retrieved from the API call to request details of each
+#    notification using the API described in:
+#      http://www.gov.hk/en/theme/psi/datasets/govhknotidetails.htm
+#
+# Your program should print a short summary of each notification in a single
+# language of your choice.
+###############################################################################

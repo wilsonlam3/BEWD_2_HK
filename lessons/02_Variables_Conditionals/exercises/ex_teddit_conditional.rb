ef295851679701ceff3ee9fa9708d8777dd61bba
WilsonKoder111 <wilson.koder.111@gmail.com>
2014-11-04 17:14:33 +0800

    added new secret num homework

--- lessons/02_Variables_Conditionals/exercises/ex_teddit_conditional.rb
+++ lessons/02_Variables_Conditionals/exercises/ex_teddit_conditional.rb
@@ -1,21 +1,23 @@
 # Teddit Contionals - Starter Code.
 
 # Let's add a new Teddit feature. Upvotes!
-# Complete the application below. 
+# Complete the application below.
 # Where you see comments (lines that begin with #) replace it with code so that the program works.
 
 def get_input
-  #Get input from the user.
+  gets
 end
 
-def calculate_upvotes(story, category)
-	# Write code so that:
-		# If the Story is about cats multiply the upvotes by 5
-		# If the Story is about bacon multiply the upvotes by 8
-		# If the Story is about Food it gets 3 times the upvotes.
-
-	#For example:
-	# "Cats frolic despite tuna shortage" should give you 5 times the upvotes!
+def calculate_upvotes(story, category, upvotes)
+    if story.include? "cats"
+        upvotes * 5
+    elsif story.include? "bacon"
+        upvotes * 8
+    elsif story.include? "food"
+        upvotes * 8
+    else
+        upvotes
+    end
 end
 
 puts "Welcome to Teddit! a text based news aggregator. Get today's news tomorrow!"
@@ -23,5 +25,5 @@ puts "Please enter a News story:"
 story = get_input
 puts "Please give it a category:"
 category = get_input
-upvotes = calculate_upvotes(story, category)
+upvotes = calculate_upvotes(story, category, 4)
 puts "New story added! #{story}, Category: #{category.capitalize}, Current Upvotes: #{upvotes}"

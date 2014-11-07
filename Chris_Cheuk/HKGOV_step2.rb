51f2088a35901e062d8c242a3de72597c6f4fa8e
Chris Cheuk <cc.cheuk@gmail.com>
2014-11-07 11:46:53 +0800

    finished API HW step 2

--- Chris_Cheuk/HKGOV_step2.rb
+++ Chris_Cheuk/HKGOV_step2.rb
@@ -1,20 +1,38 @@
 require ('rest-client')
 require 'json'
 
+
+string = RestClient.get 'http://ogcmn.one.gov.hk/ogcmn/service/list/catwc/OGCMN*?lang=en'
+string = string[12...-2]
+
+hash= Hash.new
+hash = JSON.parse(string)
+
+
+#puts hash
+#puts hash.class
+
+hash["notifications"].each do |array|
+	print "#{array["id"]}  :" 
+	puts array["subject"]  
+
+end
+
 print ("enter the id that you want to know in details: ")
-#id=gets.chomp
-stringdetail = RestClient.get 'http://ogcmn.one.gov.hk/ogcmn/service/noti/detail/7686925' 
+id=gets.chomp
+stringdetail = RestClient.get 'http://ogcmn.one.gov.hk/ogcmn/service/noti/detail/'+id 
 stringdetail= stringdetail[14...-2]
-stringdetail= stringdetail.delete! "\\n"
-stringdetail= stringdetail.delete! "\\"
+#stringdetail= stringdetail.delete! "\\n"
+#stringdetail= stringdetail.delete! "\\"
 hashdetail = JSON.parse(stringdetail)
 
-puts stringdetail
+#puts stringdetail
 #puts hashdetail
 #puts hashdetail.class
 
-#hashdetail.each do |tag|
-#	print tag["id"].to_i 
-#	puts tag["subject_en"]  
+#print "#{hashdetail["id"]}: "
+#puts hashdetail["subject_en"] 
 
-#end
+
+msgdetail=JSON.parse(hashdetail["msg_en"])
+msgdetail["body"].each {|array| puts array["val"]}

13245b14a36f6ffa17540b18b063bd3eda1a54bf
Hubert Lee <hubert.k.lee@gmail.com>
2014-11-26 22:04:47 +0800

    add fizzbuzz solution

--- tutorials/02/fizz_buzz/fizz_buzz_spec.rb
+++ tutorials/02/fizz_buzz/fizz_buzz_spec.rb
@@ -3,10 +3,16 @@ require_relative 'fizz_buzz'
 
 RSpec.describe 'fizz_buzz' do
   it 'returns Fizz for a multiple of 3' do
-    expect(fizzbuzz(9)).to eq('Fizz')
+    expect(fizz_buzz(9)).to eq('Fizz')
   end
 
-  it 'returns Buzz for a multiple of 5'
+  # This test case fails... why?
+  it 'returns Buzz for a multiple of 5' do
+    expect(fizzbuzz(10)).to eq('Buzz')
+  end
+
+  # Complete these two test cases
+
   it 'returns FizzBuzz for a multiple of 3 and 5'
   it 'returns the number when the number is not a multiple of 3 or 5'
 end
